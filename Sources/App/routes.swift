f49b8d6e64868dfb600b77f92cc89c60dce0cd55
TJ Barber <me@tjbarber.org>
2024-02-03 19:51:26 -0600

    got a working count

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,20 @@
 import Vapor
 
+class Count {
+    var count = 0
+    
+    private init() {}
+
+    static let sharedInstance = Count()
+}
+
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
+    app.get { req throws in
+        try req.view.render("index", ["count": Count.sharedInstance.count])
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.post("count") { req async throws -> View in
+       Count.sharedInstance.count += 1
+       return try await req.view.render("count", ["count": Count.sharedInstance.count])
     }
 }
